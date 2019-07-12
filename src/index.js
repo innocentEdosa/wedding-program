e3e8171d0a304d2aaad416e6ac0b145aaceb312b
innocentEdosa <45862608+innocentEdosa@users.noreply.github.com>
2019-07-12 14:53:22 +0100

    Revert "167226439 Setup-react-project-with-webpack"

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-const Index = () => <div>This is just the test div again</div>
-
-
-render(<Index/>, document.getElementById('root'));
